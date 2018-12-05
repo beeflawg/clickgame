8f6815aa52c5b5dbf0709590df8e58872506da62
beef <beeflawg@gmail.com>
2018-12-05 11:02:46 -0700

    Updates

---
+++ precache-manifest.bf2d952a55706cbec04579c1c111dca0.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "1d1da0236ccd610ca49c",
+    "url": "/clickgame/static/js/runtime~main.1d1da023.js"
+  },
+  {
+    "revision": "1220f191c7bbba19bbb3",
+    "url": "/clickgame/static/js/main.1220f191.chunk.js"
+  },
+  {
+    "revision": "f68dcc1a0c0c6e129be0",
+    "url": "/clickgame/static/js/1.f68dcc1a.chunk.js"
+  },
+  {
+    "revision": "d26f49abbac352e236622f24e50bf421",
+    "url": "/clickgame/index.html"
+  }
+];
